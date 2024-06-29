52ea3f2a4a4ca5744ce2aae629348158da6497e8
Mustafa Kerem Cansev <mkeremcansev@gmail.com>
2024-06-30 02:06:27 +0300

    main: remove loading bar

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -5,9 +5,6 @@
 <body class="ltr app sidebar-mini dark-mode">
 
 <!-- GLOBAL-LOADER -->
-<div id="global-loader">
-    <img src="{{ asset('assets/images/loader.svg') }}" class="loader-img" alt="Loader">
-</div>
 <!-- /GLOBAL-LOADER -->
 
 <!-- PAGE -->
