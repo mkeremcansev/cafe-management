ebddec38938807dfd23cc6576273f01de0195432
Mustafa Kerem Cansev <mkeremcansev@gmail.com>
2024-06-30 00:38:13 +0300

    main: add premium user

--- config/app.php
+++ config/app.php
@@ -90,6 +90,7 @@ return [
 
     'premium_users' => [
         'mkeremcansev@gmail.com',
+        'mee.cansev@gmail.com'
     ],
 
     'auto-deploy-secret' => env('AUTO_DEPLOY_SECRET', 'YLyW4xbmuqQKsGyXqGnjuN5Ak0ifmYR7ZfVKjpiOsJIjigG3vcgiqxfkHZ3Lwhbrp9h9NF6ECfS4Xp02FdRTp4eKUlBiKgsErrqA'),
