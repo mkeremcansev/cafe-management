a0e8c5696afe58a598925f6e22fbaa5076667212
Mustafa Kerem Cansev <mkeremcansev@gmail.com>
2024-06-30 00:28:55 +0300

    main: update AutoDeployController

--- app/Http/Controllers/AutoDeployController.php
+++ app/Http/Controllers/AutoDeployController.php
@@ -13,7 +13,9 @@ class AutoDeployController extends Controller
         $githubHash = $request->header('X-Hub-Signature');     $localToken = config('app.auto-deploy-secret');
         $localHash = 'sha1=' . hash_hmac('sha1', $githubPayload, $localToken, false);     if (hash_equals($githubHash, $localHash)) {
         $root_path = base_path();
-        $process = new Process('cd ' . $root_path . '; ./deploy.sh');
+        $process = new Process([
+            'cd ' . $root_path . '; ./deploy.sh'
+        ]);
         $process->run(function ($type, $buffer) {
             echo $buffer;
         });
