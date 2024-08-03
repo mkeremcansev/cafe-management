bd258108455f2a3b561ec6194239f2562338f448
Mustafa Kerem CANSEV <76810832+mkeremcansev@users.noreply.github.com>
2024-08-03 16:52:44 +0300

    main: update `StoreCategoryRequest` for unique name

--- app/Http/Requests/StoreCategoryRequest.php
+++ app/Http/Requests/StoreCategoryRequest.php
@@ -22,7 +22,7 @@ class StoreCategoryRequest extends FormRequest
     public function rules(): array
     {
         return [
-            'name' => ['required', 'string', 'max:255'],
+            'name' => ['required', 'string', 'max:255', 'unique:categories,name'],
         ];
     }
 }
