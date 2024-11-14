fc5a357e47d337476116d456440d7094246b5969
Mustafa Kerem CANSEV <76810832+mkeremcansev@users.noreply.github.com>
2024-11-14 12:26:43 +0300

    Update footer.blade.php

--- resources/views/includes/footer.blade.php
+++ resources/views/includes/footer.blade.php
@@ -2,7 +2,7 @@
     <div class="container">
         <div class="row align-items-center flex-row-reverse">
             <div class="col-md-12 col-sm-12 text-center">
-                Copyright © {{ date('Y') }} <a href="#">Rymark Works</a>.
+                Copyright © {{ date('Y') }} <a href="https://github.com/mkeremcansev">Rymark Works</a>.
             </div>
         </div>
     </div>
